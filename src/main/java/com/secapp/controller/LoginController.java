ad49284dbb0255d4a2d2e674e01791ce70cf3fbd
41540 <hegyesi.csaba@egis.hu>
2016-09-15 13:31:01 +0200

    első

---
+++ src/main/java/com/secapp/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.secapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("LoginController / hello()");
+
+        return "hello";
+
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+
+    }
+
+    @RequestMapping("/")
+    public String startIndex() {
+        return "index";
+
+    }
+
+}
