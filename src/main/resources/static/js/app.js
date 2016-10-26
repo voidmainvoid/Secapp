7ddc33b3d16b7e47eca1ec98e8f343862d31b8a5
csaba <email>
2016-10-26 17:09:28 +0200

    angular

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("airplaneApp", []);
+//angular.module('airplane', [])
+app.controller('controller1', function($scope) {
+    $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+    $scope.namex="namex iit";
+    $scope.firstname="";
+});
+
